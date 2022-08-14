0f15fec149333a66d30aaf4bfef5ae7609ff3c1a
williampereira84@gmail.com <wSilvaPereira>
2022-08-14 16:27:21 -0300

    Commit Inicial

---
+++ vendas/src/main/java/io/github/wSilvaPereira/model/Cliente.java
@@ -0,0 +1,14 @@
+package io.github.wSilvaPereira.model;
+
+public class Cliente {
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    private String nome;
+}
---
+++ vendas/src/main/java/io/github/wSilvaPereira/repository/ClientesRepository.java
@@ -0,0 +1,10 @@
+package io.github.wSilvaPereira.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+    public void persistir(Object cliente) {
+        //Acessa a base e salva o cliente
+    }
+}
