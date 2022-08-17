3a5f24ff12ea2771656b791fe5f0767752077a2a
williampereira84@gmail.com <wSilvaPereira>
2022-08-17 12:23:07 -0300

    Método getClienteById

--- vendas/src/main/java/io/github/wSilvaPereira/VendasApplication.java
+++ vendas/src/main/java/io/github/wSilvaPereira/VendasApplication.java
@@ -1,10 +1,23 @@
 package io.github.wSilvaPereira;
 
+import io.github.wSilvaPereira.domain.entity.Cliente;
+import io.github.wSilvaPereira.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
+        return args -> {
+            clientes.save(new Cliente("William"));
+        };
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
