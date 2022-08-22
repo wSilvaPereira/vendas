1c912eae58839c0a385348ec4399186c918de16a
williampereira84@gmail.com <wSilvaPereira>
2022-08-22 12:05:21 -0300

    Implementando Internacionalização

--- vendas/src/main/java/io/github/wSilvaPereira/rest/dto/PedidoDTO.java
+++ vendas/src/main/java/io/github/wSilvaPereira/rest/dto/PedidoDTO.java
@@ -14,11 +14,11 @@ import java.util.List;
 @Data
 public class PedidoDTO {
 
-    @NotNull(message = "Informe o código do cliente")
+    @NotNull(message = "{campo.codigo-cliente.obrigatorio}")
     private Integer cliente;
-    @NotNull(message = "Informe o total do pedido")
+    @NotNull(message = "{campo.total-pedido.obrigatorio}")
     private BigDecimal total;
-    @NotEmptyList(message = "Pedido não pode ser realizado sem itens")
+    @NotEmptyList(message = "{campo.items-pedido.obrigatorio}")
     private List<ItemPedidoDTO> items;
 
 }
