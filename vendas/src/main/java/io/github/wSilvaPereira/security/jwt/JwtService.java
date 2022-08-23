abd8db3a6d3dc4937b130155bf71b4041105b05f
williampereira84@gmail.com <wSilvaPereira>
2022-08-23 11:58:22 -0300

    Preparando a classe de autenticação

--- vendas/src/main/java/io/github/wSilvaPereira/security/jwt/JwtService.java
+++ vendas/src/main/java/io/github/wSilvaPereira/security/jwt/JwtService.java
@@ -1,6 +1,8 @@
-package io.github.wSilvaPereira;
+package io.github.wSilvaPereira.security.jwt;
 
 import io.github.wSilvaPereira.domain.entity.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,6 +14,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.HashMap;
 
 @Service
 public class JwtService {
@@ -36,12 +39,43 @@ public class JwtService {
                 .compact();
     }
 
+    private Claims obterClaims(String token) throws ExpiredJwtException {
+        return Jwts
+                .parser()
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean tokenValido(String token) {
+        try {
+            Claims claims = obterClaims(token);
+            Date dataExpiracao = claims.getExpiration();
+            LocalDateTime data =
+                    dataExpiracao
+                            .toInstant()
+                            .atZone(ZoneId.systemDefault()).toLocalDateTime();
+            return !LocalDateTime.now().isAfter(data);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String obterLoginUsuario(String token) throws ExpiredJwtException{
+        return (String) obterClaims(token).getSubject();
+    }
+
 //    public static void main(String[] args){
 //        ConfigurableApplicationContext contexto = SpringApplication.run(VendasApplication.class);
 //        JwtService service = contexto.getBean(JwtService.class);
 //        Usuario usuario = Usuario.builder().login("William").build();
 //        String token = service.gerarToken(usuario);
 //        System.out.println(token);
+//
+//        boolean isTokenValido =  service.tokenValido(token);
+//        System.out.println("O Token está válido? " + isTokenValido);
+//
+//        System.out.println(service.obterLoginUsuario(token));
 //    }
 
 }
