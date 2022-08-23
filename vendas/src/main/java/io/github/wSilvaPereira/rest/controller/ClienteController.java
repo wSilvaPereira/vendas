594279e90e9aaff54df01e600093ef9c1764b356
williampereira84@gmail.com <wSilvaPereira>
2022-08-23 17:08:17 -0300

    Adicionado swagger

--- vendas/src/main/java/io/github/wSilvaPereira/rest/controller/ClienteController.java
+++ vendas/src/main/java/io/github/wSilvaPereira/rest/controller/ClienteController.java
@@ -2,6 +2,7 @@ package io.github.wSilvaPereira.rest.controller;
 
 import io.github.wSilvaPereira.domain.entity.Cliente;
 import io.github.wSilvaPereira.domain.repository.Clientes;
+import io.swagger.annotations.*;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/clientes")
+@Api("Api Clientes")
 public class ClienteController {
 
     private Clientes repository;
@@ -22,7 +24,12 @@ public class ClienteController {
     }
 
     @GetMapping("{id}")
-    public Cliente getClienteById(@PathVariable Integer id) {
+    @ApiOperation("Obter detalhes de um cliente")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Cliente encontrado"),
+            @ApiResponse(code = 400, message = "Cliente não encontrado para o ID informado")
+    })
+    public Cliente getClienteById(@PathVariable  @ApiParam("Id do cliente") Integer id) {
         return repository
                 .findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
@@ -30,6 +37,11 @@ public class ClienteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation("Salva um novo cliente")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Cliente salvo com sucesso"),
+            @ApiResponse(code = 400, message = "Erro de validação")
+    })
     public Cliente save(@RequestBody @Valid Cliente cliente) {
         return repository.save(cliente);
     }
